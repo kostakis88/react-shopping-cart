ffb944851283712ad505aa4520908d4bac858932
kostakis88 <kostasdallas2010@hotmail.com>
2018-11-20 23:28:09 +0200

    using state, fragments and methods

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,13 +1,27 @@
 import React, {Component} from 'react';
 
 class Counter extends Component {
+    state = {
+        count: 0
+    };
+
     render() {
         return (
-            <div>
-                <h1>Hello World</h1>
-                <button>Increment</button>
-            </div>
+            <React.Fragment>
+                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+                <button className="btn btn-secondary btn-sm">Increment</button>
+            </React.Fragment>
         );
+    }
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += this.state.count === 0 ? "warning" : "primary";
+        return classes;
+    }
+
+    formatCount() {
+        const { count } = this.state;
+        return count === 0 ? "Zero" : count;
     }
 }
 export default Counter;
