a5f09e4e029ce8598bfa250be8db74a56ddfb66e
kostakis88 <kostasdallas2010@hotmail.com>
2018-11-23 23:36:34 +0200

    updating tha state

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,13 +2,12 @@ import React, {Component} from 'react';
 
 class Counter extends Component {
     state = {
-        count: 0,
-        tags: []
+        count: 0
     };
 
 
     handleIncrement = () => {
-        console.log("Increment Clicked", this);
+        this.setState({ count: this.state.count + 1});
     }
 
     render() {
@@ -16,17 +15,10 @@ class Counter extends Component {
             <React.Fragment>
                 <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
                 <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
-                {this.state.tags.length === 0 && "Please create a new tag!"}
-                {this.renderTags()}
             </React.Fragment>
         );
     }
 
-    renderTags() {
-        if (this.state.tags.length === 0) return <p>There are no tags!</p>;
-
-        return <ul>{ this.state.tags.map(tag => <li key={tag}>{ tag }</li>) }</ul>;
-    }
     getBadgeClasses() {
         let classes = "badge m-2 badge-";
         classes += this.state.count === 0 ? "warning" : "primary";
